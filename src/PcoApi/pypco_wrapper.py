f84174cef7dad929fba5acccafdbe6a68f0728d0
2000game <felix.kotschenreuther@gmail.com>
2023-06-29 04:06:14 +0000

    Fixed bug in pypco_wrapper during intializition of pypco

--- src/PcoApi/pypco_wrapper.py
+++ src/PcoApi/pypco_wrapper.py
@@ -19,21 +19,20 @@ class PyPcoWrapper:
         secret: Optional[str] = None,  # pylint: disable=unsubscriptable-object
         token: Optional[str] = None,  # pylint: disable=unsubscriptable-object
     ):
-        self.cc_name: Optional[str] = None,  # pylint: disable=unsubscriptable-object
-        self.api_base: str = "https://api.planningcenteronline.com",
-        self.timeout: int = 60,
-        self.upload_url: str = "https://upload.planningcenteronline.com/v2/files",
-        self.upload_timeout: int = 300,
-        self.timeout_retries: int = 3,
+        api_base: str = "https://api.planningcenteronline.com"
+        timeout: int = 60
+        upload_url: str = "https://upload.planningcenteronline.com/v2/files"
+        upload_timeout: int = 300
+        timeout_retries: int = 3
         self.pco = pypco.PCO(
             application_id=application_id,
             secret=secret,
             token=token,
-            api_base=self.api_base,
-            timeout=self.timeout,
-            upload_url=self.upload_url,
-            upload_timeout=self.upload_timeout,
-            timeout_retries=self.timeout_retries,
+            api_base=api_base,
+            timeout=timeout,
+            upload_url=upload_url,
+            upload_timeout=upload_timeout,
+            timeout_retries=timeout_retries,
         )
 
     def request_response(
