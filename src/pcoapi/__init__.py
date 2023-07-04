93a5b3938788bc679d98b9339d1cb4e057357850
2000game <felix.kotschenreuther@gmail.com>
2023-07-05 11:49:49 +1200

    Updated CI workflows

--- src/pcoapi/__init__.py
+++ src/pcoapi/__init__.py
@@ -3,9 +3,12 @@ from __future__ import annotations
 __version__ = "0.0.2"
 
 import sys
-from typing import Union
+from typing import Dict, List, Union
 
 if sys.version_info >= (3, 10):
-    JSONDict = Union[dict[str, "JSONDict"], list["JSONDict"], str, int, float, bool, None]
+    JsonValueType = (
+        dict[str, "JsonValueType"] | list["JsonValueType"] | str | int | float | bool | None
+    )
+    JSONDict = dict[str, JsonValueType]
 else:
     JSONDict = dict[str, object]
